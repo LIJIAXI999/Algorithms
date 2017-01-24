066565a4276d56a0073d82e56ff75db7df195a15
94760 <947600160@qq.com>
2017-01-24 16:53:19 +0800

    daily exercise

--- src/exercise/chapter2_2/Accumulator.java
+++ src/exercise/chapter2_2/Accumulator.java
@@ -19,16 +19,28 @@ import tools.StdRandom;
  */
 public class Accumulator {
     private double total;
+    private double m;
+    private double s;
     private int N;
     public void addDataValue(double val){
         N++;
+        s=s+1.0*(N-1)/N*(val-m)*(val-m);
+        m=m+(val-m)/N;
         total+=val;
     }
+    public double var(){
+        return s/(N-1);
+    }
+    public double stddev(){
+        return Math.sqrt(this.var());
+    }
+
     public double mean(){
         return total/N;
     }
     public String toString(){
-        return "Mean ("+N+"values):"+String.format("%7.5f",mean());
+        return "Mean ("+N+"values):"+String.format("%7.5f",mean())+"" +"\n"+
+                "var:"+var()+"\n"+"stddev:"+stddev();
     }
 
     public static void main(String[] args){
